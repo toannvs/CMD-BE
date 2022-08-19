4feab7f01542edf63562b069f50da739e933c4fe
dnguyen <nguyenminhdungtd98@gmail.com>
2022-08-19 18:11:11 +0700

    Forgot Password

--- src/main/java/com/comaymanagement/cmd/controller/AuthController.java
+++ src/main/java/com/comaymanagement/cmd/controller/AuthController.java
@@ -1,12 +1,8 @@
 package com.comaymanagement.cmd.controller;
 
-import java.net.URI;
-import java.net.http.HttpHeaders;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
