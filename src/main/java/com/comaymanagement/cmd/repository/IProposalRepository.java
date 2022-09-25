e0568dd8f21d94398f51001704669c9f08845d95
tnguyen669 <toanvocong.nguyen@dxc.com>
2022-09-25 22:00:35 +0700

    Improved proposal-findApproveByMe part performance

--- src/main/java/com/comaymanagement/cmd/repository/IProposalRepository.java
+++ src/main/java/com/comaymanagement/cmd/repository/IProposalRepository.java
@@ -2,13 +2,14 @@ package com.comaymanagement.cmd.repository;
 
 import java.util.List;
 
+import com.comaymanagement.cmd.entity.ApprovalStep;
 import com.comaymanagement.cmd.entity.Proposal;
 import com.comaymanagement.cmd.entity.ProposalDetail;
 import com.comaymanagement.cmd.model.ProposalModel;
 
 
 public interface IProposalRepository {
-	public List<ProposalModel> findAllProposalApproveByMe(Integer employeeId,List<Integer> proposalTypeIds, List<Integer> statusId, List<Integer> creatorIds,
+	public List<ProposalModel> findAllProposalApproveByMe(List<ApprovalStep> appSteps,List<Integer> proposalTypeIds, List<Integer> statusId, List<Integer> creatorIds,
 			String createDate, String finishDate, String sort, String order, Integer offset, Integer limit);
 	public Integer countAllPaging(Integer employeeId,Integer proposal, String content, String status, String creator,
 			String createDateFrom, String createDateTo, String sort, String order, Integer offset, Integer limit);
